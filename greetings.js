ce2f6e1406fb99064cede6a7c7bc931fff8404c0
prachialex <prachialex21@gmail.com>
2021-05-10 18:42:31 +0530

    greetings

---
+++ greetings.js
@@ -0,0 +1,17 @@
+let output="";
+function greet(userType,firstName,lastName="")
+{
+    switch(userType)
+{
+	case 1: output="Thank you " + firstName+""+lastName+ " for your validation";
+	break;
+	case 2: output="Thank you " + firstName+""+lastName+ " for keeping the network secure";
+	break;
+	case 3: output="Thank you " + firstName+""+lastName+ " for using the platform";
+	break;
+	default: output="Enter valid input ";
+    break;
+}
+console.log(output);
+}
+greet(1,"Prachi");
